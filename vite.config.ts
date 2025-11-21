7106bc0302c7e5425f88d8b77e5518ac420e9874
MrDiamondDog <84212701+MrDiamondDog@users.noreply.github.com>
2025-11-20 20:16:23 -0700

    grr

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,11 @@ import type { UserConfig } from 'vite'
 
 export default {
     server: {
-        host: true
+        host: true,
+        port: 80,
     },
     preview: {
-        host: true
+        host: true,
+        port: 80,
     }
 } satisfies UserConfig;
